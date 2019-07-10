af7a1f56afca20ea22fce77919623960d5b20b49
Darrengobel <Darren.Gobel@Gmail.com>
2019-07-10 14:45:10 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import Content from './components/Content'
+import Counter from './components/Counter'
+
+const initialState = {
+  count : 0
+}
+
+const reducer = (state = initialState, action = {}) => {
+  switch (action.type){
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      }
+      case 'DECREMENT' :
+      return {
+        count: state.count - 1
+      }
+      default:
+        return state
+  }
+}
+
+const reduxStore = createStore (
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+const App = () => {
+  return (
+    <Provider store={reduxStore}>
+      <div>
+        <Content />
+        <Counter />
+      </div>
+      </Provider>
+  )
+  }
+
+export default App
